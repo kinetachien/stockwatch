d86d5e47c11762f0c1ca7298dfd7609e6fa5b46a
kinetachien <kineta.chien@gmail.com>
2021-03-01 19:01:14 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const exphbs = require("express-handlebars");
+const path = require("path");
+
+const PORT = process.env.PORT || 5000;
+
+// Set Handlebars Middleware
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+const otherstuff = "Kineta is learning node.js";
+
+// Set Handlebar routes
+app.get("/", function (req, res) {
+  res.render("home", {
+    stuff: "this is stuff",
+    intent: otherstuff,
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => console.log("Server listening on port " + PORT));
